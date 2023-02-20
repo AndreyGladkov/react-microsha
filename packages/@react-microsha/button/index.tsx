046286a083d53dae8302de462c19963e1030023d
AndreyGladkov <andrewgladkovlz@gmail.com>
2023-02-20 18:10:18 +0300

    temp

--- packages/@react-microsha/button/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, PropsWithChildren } from 'react';
-
-const Button: FC<PropsWithChildren> = ({ children, ...props }) => {
-    return <button {...props}>{children}</button>;
-};
-
-export default Button;
